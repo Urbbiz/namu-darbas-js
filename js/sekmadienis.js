44a3a12abbcf1a75262d79f22e80d60c090edd3c
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2020-11-15 17:18:47 +0200

    juodrasciai2

---
+++ js/sekmadienis.js
@@ -0,0 +1,25 @@
+/*const zodis = `pomidoras`;
+const step = 2;
+const pirmosRaidesPozicija = step-1;
+let rez ='';
+
+for (let i =step-1; i < zodis.length; i= i+step) {
+    const raide = zodis[i]; 
+    rez = rez+ raide;
+
+    console.log(raide);
+  
+}
+*/
+
+const zodis ='kibirelis'
+const zingsnis = 2;
+const pirmosRaidesPozicija= zingsnis -1;
+let rezultatas = '';
+
+for (let i =pirmosRaidesPozicija; i < zodis.length; i= i+zingsnis){
+    const raide = zodis[i];
+    rezultatas=rezultatas +raide;
+   
+}
+console.log (rezultatas);
